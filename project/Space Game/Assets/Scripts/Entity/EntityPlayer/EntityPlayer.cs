3b248756d64e2349666fba3b84fb8be203cbe4ff
HudsonGTV <HudsonGTV@users.noreply.github.com>
2018-12-15 16:53:08 -0800

    fixed movement stuff and added camera movement

--- project/Space Game/Assets/Scripts/Entity/EntityPlayer/EntityPlayer.cs
+++ project/Space Game/Assets/Scripts/Entity/EntityPlayer/EntityPlayer.cs
@@ -11,7 +11,13 @@ public class EntityPlayer : MonoBehaviour {
 	void Update() {
 
 		// END GAME KEY
-		
+		if(Input.GetKey("escape")) {
+			#if UNITY_EDITOR
+				UnityEditor.EditorApplication.isPlaying = false;
+			#else
+				Application.Quit();
+			#endif
+		}
 
 	}
 
--- project/Space Game/Assets/Scripts/Entity/EntityPlayer/PlayerCameraController.cs
+++ project/Space Game/Assets/Scripts/Entity/EntityPlayer/PlayerCameraController.cs
@@ -2,13 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour {
+public class PlayerCameraController : MonoBehaviour {
+
+	public float Sensitivity = 100.0f;
+
+	public float Pitch = 0.0f;
+	public float Yaw = 0.0f;
 
 	void Start() {
 
 	}
 
 	void Update() {
+
+		// LOCK CURSOR
+		Cursor.lockState = CursorLockMode.Locked;
+
+		// CAMERA MOVEMENT
+		var mouseX = Input.GetAxis("Mouse X");
+		var mouseY = Input.GetAxis("Mouse Y");
+
+		Yaw += mouseX * Time.deltaTime * Sensitivity;
+		Pitch -= mouseY * Time.deltaTime * Sensitivity;
+
+		transform.eulerAngles = new Vector3(0.0f, Yaw, 0.0f);
+		transform.GetChild(0).eulerAngles = new Vector3(Pitch, Yaw, 0.0f);
 
 	}
 
